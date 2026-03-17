9cf133ab892f7be808e8f24025f0f5501e927084
Sai-Eshwar-Supreet <saieshwarsupreet@gmail.com>
2026-03-17 02:01:44 +0000

    Cleanup and move files to proper folders

--- src/game/Card.jsx
+++ src/game/Card.jsx
@@ -1,4 +1,4 @@
-import { buildURL } from "./ImageProvider";
+import { buildURL } from "./utils/ImageProvider";
 
 function Card({id, handleCardClick}){
     return <img onClick={handleCardClick} data-id={id} src={buildURL(id)} alt="Game card" />
